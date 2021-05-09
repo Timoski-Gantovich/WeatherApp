7f60f9bc1986271a5e6d77a1e9abcba064b76f40
Timoski-Gantovich <61708097+Timoski-Gantovich@users.noreply.github.com>
2021-05-08 20:49:26 -0700

    started

---
+++ js/script.js
@@ -0,0 +1,21 @@
+// *Your* API KEY
+const APIKEY = "cd71b381a8cede24534272a932e79c73";
+const city = "Post Falls";
+const url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${APIKEY}&units=imperial`;
+
+// check-check
+console.log(url);
+
+// Using fetch to get data
+fetch(url)
+  .then((response) => response.json())
+  .then((data) => {
+    // check-check
+    console.log(data.main.temp);
+
+    const c = document.querySelector(".city");
+    const temp = document.querySelector(".temp");
+
+    c.textContent = data.name;
+    temp.textContent = data.main.temp;
+  });
