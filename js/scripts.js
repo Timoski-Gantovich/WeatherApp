f282f6e1e0efff97fa88aa9914486247769652db
Timoski-Gantovich <61708097+Timoski-Gantovich@users.noreply.github.com>
2021-06-02 15:43:00 -0700

    general mop up

--- js/scripts.js
+++ js/scripts.js
@@ -1,29 +1,44 @@
-function weather(cityID) {
-  var key = "cd71b381a8cede24534272a932e79c73";
-  fetch(
-    "https://api.openweathermap.org/data/2.5/weather?id=" +
-      cityID +
+function getWeather() {
+  let temperature = document.getElementById("temperature");
+  let description = document.getElementById("description");
+  let location = document.getElementById("location");
+
+  let api = "https://api.openweathermap.org/data/2.5/weather";
+  let apiKey = "cd71b381a8cede24534272a932e79c73";
+
+  location.innerHTML = "Locating...";
+
+  navigator.geolocation.getCurrentPosition(success, error);
+
+  function success(position) {
+    var latitude = position.coords.latitude;
+    var longitude = position.coords.longitude;
+
+    let url =
+      api +
+      "?lat=" +
+      latitude +
+      "&lon=" +
+      longitude +
       "&appid=" +
-      key
-  )
-    .then(function (resp) {
-      return resp.json();
-    }) // Convert data to json
-    .then(function (data) {
-      getWeather(data);
-    })
-    .catch(function () {
-      // catch any errors
-    });
+      apiKey +
+      "&units=imperial";
+
+    fetch(url)
+      .then((response) => response.json())
+      .then((data) => {
+        console.log(data);
+        let temp = data.main.temp;
+        temperature.innerHTML = temp + "° F";
+        location.innerHTML =
+          data.name + " (" + latitude + "°, " + longitude + "°)";
+        description.innerHTML = data.weather[0].main;
+      });
+  }
+
+  function error() {
+    location.innerHTML = "Unable to retrieve your location";
+  }
 }
 
-window.onload = function () {
-  weather(5604353);
-};
-function getWeather(d) {
-  var fahrenheit = Math.round((parseFloat(d.main.temp) - 273.15) * 1.8 + 32);
-
-  document.getElementById("description").innerHTML = d.weather[0].description;
-  document.getElementById("temp").innerHTML = fahrenheit + "&deg;";
-  document.getElementById("location").innerHTML = d.name;
-}
+getWeather();
